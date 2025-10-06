744f5a5a8f8384c7fed7235ce92d4b590c4c0d7c
Chris Bolton <cbolton@landscapefx.com>
2025-10-06 08:55:37 -0400

    Re order links and add a couple new ones

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import './App.css'
 import { FaUserTie, FaFileInvoiceDollar, FaThLarge, FaHeadset } from 'react-icons/fa'
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import { FaCameraRetro } from 'react-icons/fa';
-import { FaPaintBrush } from 'react-icons/fa';
-import { FaAddressCard } from "react-icons/fa";
-import { FaRegSnowflake } from "react-icons/fa";
-import { FaCar } from "react-icons/fa";
-import { MdAccessTimeFilled } from "react-icons/md";
-import { MdOutlineMiscellaneousServices } from "react-icons/md";
+import { FaCameraRetro, FaPaintBrush, FaAddressCard, FaRegSnowflake, FaCar } from 'react-icons/fa';
+import { MdAccessTimeFilled, MdOutlineMiscellaneousServices } from "react-icons/md";
+import { AiFillPicture } from "react-icons/ai";
 import CSConcreteLogo from './assets/CSConcreteGroup.png';
 import RPLogo from './assets/RP_logo.png';
 import SupplyLogo from './assets/LFXSupply.png';
 import DriveLogo from './assets/TheDRIVE.png';
 import GroupLogo from './assets/RedTree.png';
-
 
 /* Reusable external link component */
 function ExternalLink({ url, children }) {
@@ -31,90 +26,76 @@ function ExternalLink({ url, children }) {
   );
 }
 
-/* Internal link that opens routes in a new tab */
-function InternalLink({ to, children }) {
-  return (
-    <div
-      onClick={() => window.open(to, "_blank", "noopener,noreferrer")}
-      className="link-card"
-      style={{ cursor: "pointer" }}
-    >
-      {children}
-    </div>
-  );
-}
-
 function Home() {
   return (
     <div className="container">
       <h1>LFX HQ Resources</h1>
 
-      {/* First section with your real links */}
+      {/* ---------- ROW 1 ---------- */}
       <div className="links">
-        <ExternalLink url="https://lf01849.purelyhr.com/?returnUrl=https%3a%2f%2flf01849.purelyhr.com%2fcpanel%2fcalendar.aspx">
-          <FaUserTie />
-          <span>PurelyHR</span>
-        </ExternalLink>
-
-        <ExternalLink url="https://app.yetisnow.com/dispatch">
-          <FaRegSnowflake />
-          <span>Yeti</span>
-        </ExternalLink>
-
-        <ExternalLink url="https://cloud.samsara.com/o/5002598/fleet/list?selectedSortKey=alphaAsc">
-          <FaCar />
-          <span>Samsara</span>
-        </ExternalLink>
-
         <ExternalLink url="https://time.landscapefx.com/">
-          <MdAccessTimeFilled />
-          <span>Punch In/Out</span>
+          <MdAccessTimeFilled /><span>Punch In/Out</span>
+        </ExternalLink>
+
+        <ExternalLink url="https://online.adp.com/signin/v1/?APPID=Pin4NAS">
+          <FaFileInvoiceDollar /><span>ADP</span>
+        </ExternalLink>
+
+        <ExternalLink url="https://lf01849.purelyhr.com/?returnUrl=https%3a%2f%2flf01849.purelyhr.com%2fcpanel%2fcalendar.aspx">
+          <FaUserTie /><span>PurelyHR</span>
+        </ExternalLink>
+
+        <ExternalLink url="https://platform.risepeople.com/app/#/auth/">
+          <FaUserTie /><span>Rise</span>
         </ExternalLink>
 
         <ExternalLink url="https://landscapefx-my.sharepoint.com/:x:/p/cbolton/EWlnAYRr4IFOuh_I1Lgmrc8B-a2ikYAThTZwy8cdvDRzdA?e=bS4VN0">
-          <FaFileInvoiceDollar />
-          <span>Expense Report</span>
-        </ExternalLink>
-
-        <ExternalLink url="https://www.lfxsupplycentre.com/web/login#cids=6&action=menu">
-          <FaThLarge />
-          <span>Odoo</span>
-        </ExternalLink>
-
-        <a href="mailto:help@landscapefx.com" className="link-card">
-          <FaHeadset />
-          <span>Help Desk</span>
-        </a>
-
-
-        <ExternalLink url="https://online.adp.com/signin/v1/?APPID=Pin4NAS&productId=80e309c3-709e-bae1-e053-3505430b5495&returnURL=https://signin.adp.com&callingAppId=landing&TARGET=-SM-HTTPS%3a%2f%2fsignin%2eadp%2ecom%2femployee%3ftransId%3de9677262-334f-4ac8-a64a-ba71caadfc58">
-          <FaFileInvoiceDollar />
-          <span>ADP</span>
-        </ExternalLink>
-
-        <ExternalLink url="https://forms.office.com/pages/responsepage.aspx?id=nFV4rvechk2pmiSX97OmhV21RcM1tnxJgEkvZOrjMGpURU41V0NOSVVDVVFSUVY0NTgxNDVNMzNQMi4u&route=shorturl">
-          <FaPaintBrush />
-          <span>Creative Request</span>
-        </ExternalLink>
-
-        <ExternalLink url="https://forms.office.com/pages/responsepage.aspx?id=nFV4rvechk2pmiSX97OmhV21RcM1tnxJgEkvZOrjMGpURFU2Wk1KNkVCVEFDMzhQVDdTRk81TzFCQS4u&route=shorturl">
-          <FaCameraRetro />
-          <span>Photography Request</span>
-        </ExternalLink>
-
-        <ExternalLink url="https://forms.office.com/pages/responsepage.aspx?id=nFV4rvechk2pmiSX97OmhV21RcM1tnxJgEkvZOrjMGpUNkY1TEpCR0I5RUcxVzI0QVVUMzMyREtXWC4u&route=shorturl">
-          <FaAddressCard />
-          <span>Business Card Request</span>
-        </ExternalLink>
-
-        <ExternalLink url="https://form.jotform.com/63543736394262">
-          <MdOutlineMiscellaneousServices />
-          <span>Equip Service / Damage</span>
+          <FaFileInvoiceDollar /><span>Expense Report</span>
         </ExternalLink>
       </div>
 
+      {/* ---------- ROW 2 ---------- */}
+      <div className="links">
+        <a href="mailto:help@landscapefx.com" className="link-card">
+          <FaHeadset /><span>IT Help Desk</span>
+        </a>
 
-      {/* Section title for group of companies */}
+        <ExternalLink url="https://forms.office.com/pages/responsepage.aspx?id=nFV4rvechk2pmiSX97OmhV21RcM1tnxJgEkvZOrjMGpURU41V0NOSVVDVVFSUVY0NTgxNDVNMzNQMi4u&route=shorturl">
+          <FaPaintBrush /><span>Creative Request</span>
+        </ExternalLink>
+
+        <ExternalLink url="https://forms.office.com/pages/responsepage.aspx?id=nFV4rvechk2pmiSX97OmhV21RcM1tnxJgEkvZOrjMGpURFU2Wk1KNkVCVEFDMzhQVDdTRk81TzFCQS4u&route=shorturl">
+          <FaCameraRetro /><span>Photography Request</span>
+        </ExternalLink>
+
+        <ExternalLink url="https://forms.office.com/pages/responsepage.aspx?id=nFV4rvechk2pmiSX97OmhV21RcM1tnxJgEkvZOrjMGpUNkY1TEpCR0I5RUcxVzI0QVVUMzMyREtXWC4u&route=shorturl">
+          <FaAddressCard /><span>Business Card Request</span>
+        </ExternalLink>
+
+        <ExternalLink url="https://landscapefx.sharepoint.com/:f:/s/TheDriveStudio/EjmjYshY-NNHl5sY6XaehMQB8LGZVGrmmd8sKpZFjMwOwQ?e=IWCDI6">
+          <AiFillPicture /><span>Logos</span>
+        </ExternalLink>
+      </div>
+
+      {/* ---------- ROW 3 ---------- */}
+      <div className="links">
+        <ExternalLink url="https://www.lfxsupplycentre.com/web/login#cids=6&action=menu">
+          <FaThLarge /><span>Odoo</span>
+        </ExternalLink>
+
+        <ExternalLink url="https://app.yetisoftware.com/dispatch">
+          <FaRegSnowflake /><span>Yeti</span>
+        </ExternalLink>
+
+        <ExternalLink url="https://cloud.samsara.com/o/5002598/fleet/list?selectedSortKey=alphaAsc">
+          <FaCar /><span>Samsara</span>
+        </ExternalLink>
+
+        <ExternalLink url="https://form.jotform.com/63543736394262">
+          <MdOutlineMiscellaneousServices /><span>Equip Service / Damage</span>
+        </ExternalLink>
+      </div>
+
       <h1 className="companies-title">LFX Group Of Companies</h1>
       <div className="links">
         <ExternalLink url="https://www.landscapefx.com/">
@@ -137,12 +118,9 @@ function Home() {
           <img src={DriveLogo} alt="The Drive" className="card-img" />
         </ExternalLink>
       </div>
-
-
     </div>
   );
 }
-
 
 function App() {
   return (
